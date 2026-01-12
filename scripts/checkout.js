56a4400e3afecd5a8084b07bf0b0e388af287382
Avocado <kahirocookie@gmail.com>
2026-01-12 12:48:55 +0630

    creating class to genrate objects in oop

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,6 +1,6 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js"; // this run the function you put in this --> { function }
-import '../data/cart-oop.js'; // this run all inside the file
+import '../data/cart-class.js'; // this run all inside the file
 
 renderOrderSummary();
 renderPaymentSummary();
